8dfef701758f9ff6951563695d6b1d631743d12d
阿沁 <youziyouzishu@gmail.com>
2025-07-01 11:37:49 +0800

    初始化

---
+++ config/route.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+use Webman\Route;
+
+Route::fallback(function () {
+    if (request()->header('accept') == 'application/json'){
+        throw new \Tinywan\ExceptionHandler\Exception\RouteNotFoundException();
+    }else{
+        return new \support\Response(404, [], file_get_contents(base_path('plugin' . DIRECTORY_SEPARATOR. 'admin' . DIRECTORY_SEPARATOR . 'public') . '/demos/error/404.html'));
+    }
+});
+
+
+
+
---
+++ plugin/admin/config/database.php
@@ -0,0 +1,19 @@
+<?php
+return  [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => '122.114.237.95',
+            'port'        => '3306',
+            'database'    => '0604guyi_95_hzgq',
+            'username'    => '0604guyi_95_hzgq',
+            'password'    => 'KYZjAAsP6JznAk3P',
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_general_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+    ],
+];
